6da46c5dde9ce8454531452016e46f1e25be00ab
Farrukh Rafiq <frafiq@wd311a19.local>
2017-04-25 20:09:43 -0400

    Initial commit

---
+++ src/main/java/com/frank/Main.java
@@ -0,0 +1,32 @@
+package com.frank;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+class  Task{
+    public String body;
+    public  boolean done;
+    public  int id;
+    public  int priority;
+    public  String title;
+}
+class TaskCollections{
+    public  List<Task> todos;
+}
+/**
+ * Created by frafiq on 4/25/17.
+ */
+public class Main {
+    public static void main(String[] args){
+        String
+    }
+
+
+}
